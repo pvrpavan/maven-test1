3b408065e023480a1c9aae62df0c6296ca8c60b7
pvrpavan <pvrpavan.25@gmail.com>
2025-09-29 23:12:42 +0530

    PAVANPVR0001 - Wrote the code for finding the sum of two numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,13 @@
+package com.example.javamavenjunithelloworld;
+
+public class utility {
+    public static void main(String[] args) {
+        int sum = pavanpvr(5, 3);
+        System.out.println("Sum is: " + sum);
+    }
+
+    public static int pavanpvr(int a, int b) {
+        System.out.println("This is pavanpvr");
+        return a + b;
+    }
+}
